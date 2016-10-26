6521fd1936e4e54995358c1b95579c2977ae0fd3
lramirez <lramirez@192.168.10.105>
2016-10-25 19:39:43 -0500

    Primer Commit

---
+++ comercio-jee/src/main/java/com/uan/comercio_jee/App.java
@@ -0,0 +1,13 @@
+package com.uan.comercio_jee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
